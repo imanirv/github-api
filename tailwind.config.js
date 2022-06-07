9c2c02698c9820d86ce1672e4536fe0cbf6d2543
imanirv <imanirvansyaah@gmail.com>
2022-06-07 07:51:31 +0700

    add and set tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        dark: {
+          1:"#222222",
+          2:"#292929",
+        },
+        disabled: 'rgba(217, 217, 217, 0.6)',
+      }
+    },
+  },
+  plugins: [],
+}
